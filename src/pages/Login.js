9b2094f968ebeb8001a135baa03b0e7872990250
valdeilson <valdeilson.ramos@ifp.edu.br>
2019-08-20 17:06:26 -0300

    adicionando páginas

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import './Login.css';
 import logo from "../assets/logo.svg";
+import api from "../services/api";
+ 
 
-export default function Login () {
+export default function Login ({ history }) {
+    const [ username, setUsername ] = useState('');   //variável que armazenará o usuário digitado no formulário
+    async function handleSubmit(e){
+        e.preventDefault();
+
+        const response = await api.post('/devs', {
+            username,
+        });
+        console.log(response);
+
+        history.push('/main');
+    }
+
     return (
         <div className="login-container">
-            <form>
+            <form onSubmit={ handleSubmit }>
                 <img src={logo} alt="Tindev" />
                 <input
-                    placeholder="Digite seu usuario no Github"                    
+                    placeholder="Digite seu usuario no Github"
+                    value={ username }
+                    onChange = { e => setUsername(e.target.value)}                    
                 />
                 <button type="submit" >Enviar </button>
             </form>
--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,14 @@ import Login from "./pages/Login";
 import Main from "./pages/Main";
 
 export default function Routes() {
+    
+
     return (
          <BrowserRouter>
-            <Route path= "/" component={ Login } />
+            <Route path= "/" exact component={ Login } />
             <Route path= "/main" component={ Main } />
          </BrowserRouter>
 
-    )
+    );
 
 }
