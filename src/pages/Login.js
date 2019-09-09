a0393a13d9553b02c0ed7a6a76db0869347554b7
valdeilson <vautronica@gmail.com>
2019-09-09 00:58:40 -0300

    ajustando erros no Main

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,37 +1,36 @@
 import React, { useState } from "react";
-import './Login.css';
+import "./Login.css";
 import logo from "../assets/logo.svg";
-import api from "../services/api"; 
- 
+import api from "../services/api";
 
-export default function Login ({ history }) {
-    const [ username, setUsername ] = useState('');   //variável que armazenará o usuário digitado no formulário
-    async function handleSubmit(e){
-        e.preventDefault();
+export default function Login({ history }) {
+  const [username, setUsername] = useState(""); //variável que armazenará o usuário digitado no formulário
+  async function handleSubmit(e) {
+    e.preventDefault();
 
-        const response = await api.post('/devs', {
-            username,
-        });
+    const response = await api.post("/devs", {
+      username
+    });
 
-        //console.log(response);
+    console.log(response); // depois de testar se retorna os dados, tem que comentar novamente. 46:25
 
-        const { _id } = response.data;
+    const { _id } = response.data;
 
-        history.push(`/dev/${_id}`);
-    }
+    history.push(`/dev/${_id}`);
+    //history.push("/main"); descomentar após testar o roteamento 47:40
+  }
 
-    return (
-        <div className="login-container">
-            <form onSubmit={ handleSubmit }>
-                <img src={logo} alt="Tindev" />
-                <input
-                    placeholder="Digite seu usuario no Github"
-                    value={ username }
-                    onChange = { e => setUsername(e.target.value)}                    
-                />
-                <button type="submit" >Enviar </button>
-            </form>
-        </div>
-
-    );
+  return (
+    <div className="login-container">
+      <form onSubmit={handleSubmit}>
+        <img src={logo} alt="Tindev" />
+        <input
+          placeholder="Digite seu usuario no Github"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+        <button type="submit">Enviar </button>
+      </form>
+    </div>
+  );
 }
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,78 +1,70 @@
 import React, { useEffect, useState } from "react";
-import { Link } from 'react-router-dom';
-import './Main.css';
-import api from '../services/api';
+import { Link } from "react-router-dom";
+import "./MainStyle.css";
+import api from "../services/api";
 import logo from "../assets/logo.svg";
 import like from "../assets/like.svg";
 import dislike from "../assets/dislike.svg";
 
-
 export default function Main({ match }) {
-    const [ users, setUsers ] = useState([]);
+  const [users, setUsers] = useState([]);
 
-    useEffect(() => {
-        async function loadUsers(){
-            const response = await api.get('/devs', {
-                headers: { 
-                    user: match.params.id,
-                }
-            })
-            setUsers(response.data);
+  useEffect(() => {
+    async function loadUsers() {
+      const response = await api.get("/devs", {
+        headers: {
+          user: match.params.id
         }
-        loadUsers();
-    }, [match.params.id])
-
-    async function handleLike(id){
-        await api.post(`/devs/${id}/likes`, null, {
-            headers: { user: match.params.id },
-        })
-       setUsers(users.filter(user => user._id !== id)); 
+      });
+      setUsers(response.data);
     }
+    loadUsers();
+  }, [match.params.id]);
 
-    }
+  async function handleLike(id) {
+    await api.post(`/devs/${id}/likes`, null, {
+      headers: { user: match.params.id }
+    });
 
-    async function handleDislike(id){
-        await api.post(`/devs/${id}/dislikes`, null, {
-            headers: { user: match.params.id },
-        })
-       setUsers(users.filter(user => user._id !== id)); 
-    }
+    setUsers(users.filter(user => user._id !== id));
+  }
 
+  async function handleDislike(id) {
+    await api.post(`/devs/${id}/dislikes`, null, {
+      headers: { user: match.params.id }
+    });
 
-    return (
-        <div className="main-container">
-            <Link to="/">
-            <img src={ logo } alt="Tindev" />
-            </Link>
-        
-            { users.length > 0 ? (
-                <ul>
-                    { use.map()user => (
-                    <li key={ use._id }>
-                    <img src={ use.avatar } alt={ use.name } />
-                    <footer>
-                        <strong>{ user.name }</strong>
-                        <p>{ use.bio }</p>
-                    </footer>
-                    <div className="buttons">
-                        <button type="button" onClick={() => handleDislike(user._id) }>
-                            <img src= { dislike } alt="dislike" />
-                        </button>
-                        <button type="button" onClick={() => handleLike(user._id) }>
-                            <img src= { like } alt="like" />
-                        </button>
-                    </div>
-                    </li>
-                ) }
+    setUsers(users.filter(user => user._id !== id));
+  }
 
-                </ul>
-            ): (
-                <div className="empty">Acabou </div>
-            ) }
-
-                
-        </div>
-
-    )
-
-    }
+  return (
+    <div className="main-container">
+      <Link to="/">
+        <img src={logo} alt="Tindev" />
+      </Link>
+      {users.length > 0 ? (
+        <ul>
+          {users.map(user => (
+            <li key={user._id}>
+              <img src={user.avatar} alt={user.name} />
+              <footer>
+                <strong>{user.name}</strong>
+                <p>{user.bio}</p>
+              </footer>
+              <div className="buttons">
+                <button type="button" onClick={() => handleDislike(user._id)}>
+                  <img src={dislike} alt="Dislike" />
+                </button>
+                <button type="button" onClick={() => handleLike(user._id)}>
+                  <img src={like} alt="Like" />
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className="empty"> Acabou :( </div>
+      )}
+    </div>
+  );
+}
